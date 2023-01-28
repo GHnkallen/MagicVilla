0d96a7ec0e1491604adbf785353be4875af927f0
Zhen Yao <zxyallen@gmail.com>
2023-01-27 19:58:36 -0800

    added get by id

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,32 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+
+            return VillaStore.villaList;
+
+            
+        }
+
+        [HttpGet("id:int")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
+        
+        
+
+    }
+}
